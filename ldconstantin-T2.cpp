ff0fbebcfab73ecd85d31b2ae2ef53ece6d415d8
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2015-07-06 16:38:57 -0300

    removed .in and .sol files; modified header

--- ldconstantin-T2.cpp
+++ ldconstantin-T2.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
+#include<map>
+#include<algorithm>
+
+int M;
 
 using namespace std;
 
+typedef vector<int> vi;
+
+
+struct bpnode{
+	vi ks,  /* */
+	   pos; /* */
+	int p;
+}
+
+
 int main(int argc, char **argv){
+	M = 5;
 	
 	return 0;
 }
